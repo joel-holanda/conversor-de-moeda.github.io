81ae0a78161865a60d0eba941c2fec0743012dc7
joel-holanda <joelholanda45@gmail.com>
2021-11-12 19:49:31 -0300

    primeira versao do cenversor de moeda

---
+++ conversor.js
@@ -0,0 +1,21 @@
+function calcular(){
+    let verificar = document.getElementById('valor').value
+    if(verificar == ''){
+        alert('Digite um valor')
+    }else{
+        function pegarDolar() {
+        fetch('https://economia.awesomeapi.com.br/last/USD-BRL')
+        .then((resposta) => resposta.json())
+        .then((data) => pegarvalor(data.USDBRL.ask))
+    }
+    pegarDolar()
+
+    function pegarvalor(valor){
+        let valorDolar = parseFloat(valor)
+        let valorReduzido = valorDolar.toFixed(2)             
+        let valorUsuario = document.getElementById('valor').value
+        let total =  valorUsuario * valorReduzido
+        document.querySelector('h1').innerHTML = total
+    }}
+
+}
